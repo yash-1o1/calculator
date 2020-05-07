54e2f6b3840e780ea37bd7e48be9354273658490
Yash singh <60495222+yashv99@users.noreply.github.com>
2020-05-06 20:21:51 -0400

    Taking inputs from the user

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,4 @@
-x = 1
-if x == 1:
-    # indented four spaces
-    print("x is 1.")
+first = input('Choose a number')
+second = input('Choose the second number')
+operation = input("Choose an operation to input (You can choose 'add', 'sub', 'multi' , 'div'")
+//print ('Number%s \n' % (operation))
