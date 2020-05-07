6ae0f9bb48268e5dd536b964406bb320edae31ab
Yash singh <60495222+yashv99@users.noreply.github.com>
2020-05-06 22:26:21 -0400

    Addition added tot the code now

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,12 @@
-first = input('Choose a number')
-second = input('Choose the second number')
-operation = input("Choose an operation to input (You can choose 'add', 'sub', 'multi' , 'div'")
-//print ('Number%s \n' % (operation))
+first = input('Choose a number: ')
+second = input('Choose the second number: ')
+operation = input("Choose an operation to input (You can choose 'add', 'sub', 'multi' , 'div')")
+if operation == "add": 
+    output  = int(first) + int(second)
+print ('Number%s \n' % (output))
+
+
+
+
+    
+    
